b3d57b1adff66b71b2a8cb16ad2f924a9272743a
Samahab77 <s.bagido@gmail.com>
2020-02-21 17:41:06 +0300

    done with ARIA roles

--- js/main.js
+++ js/main.js
@@ -179,6 +179,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-labelledby', `View details about ${restaurant.name}`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
